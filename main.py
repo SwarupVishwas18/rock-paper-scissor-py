f9852fc77967d72f715b86efd25025b4be256a64
SwarupVishwas18 <swarupvishwas000@gmail.com>
2022-08-15 02:12:03 +0530

    Added colors

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from random import *
-
+from colorama import Fore
+from time import sleep
 # Project Name : ROCK PAPER SCISSOR
 # Name of Developer : Swarup Deepak Vishwas
 # Date : 21-Jan-2022
 
 # ########################## GLOBAL VARIABLES #########################
+
+print(Fore.MAGENTA)
 
 gap_denoter = "----------------------------------------------------"
 
@@ -28,13 +31,17 @@ tie_num = 0
 
 def randChoice():
     choice = randint(1, 3)
+    sleep(1)
     if choice == 1:
+        print(Fore.GREEN)
         print("Bella's choice is Rock  ")
         return 'R'
     elif choice == 2:
+        print(Fore.WHITE)
         print("Bella's choice is Paper ")
         return 'P'
     elif choice == 3:
+        print(Fore.LIGHTBLUE_EX)
         print("Bella's choice is Scissor ")
         return 'S'
     else:
@@ -44,6 +51,7 @@ def randChoice():
 
 
 def code_logic(user, bella):
+    sleep(1)
     if bella == 'R':
         if user == 'R':
             return 'Tie'
@@ -76,7 +84,8 @@ print(gap_denoter)
 
 # ########################## TAKE USER NAME #########################
 
-user_name = input("But what is your name : ")
+print(Fore.CYAN)
+user_name = input("But what is your name : ").title()
 print(gap_denoter)
 print("\nHi, ", user_name, " I am HBP, organizer of ROCK PAPER SCISSOR game")
 print(gap_denoter)
@@ -84,34 +93,41 @@ print(gap_denoter)
 # ########################## TAKE USER CHOICE #########################
 
 while round <= 5:
-
-    print(gap_denoter)
+    print(Fore.YELLOW)
     print(gap_denoter)
     print("ROUND ", round)
-    print(gap_denoter)
     print(gap_denoter)
 
     user_choice = input('What is Your Choice(r/p/s) : ')
     user_choice = user_choice.upper()  # convert choice to upper case
+    if not (user_choice == 'R' or user_choice == 'P' or user_choice == 'S'):
+        print(Fore.RED)
+        print("You Gave Wrong Option Mate..!!")
+        continue
     bella_choice = randChoice()
 
     result = code_logic(user=user_choice, bella=bella_choice)
-
+    sleep(3)
     if result == 'Win':
         user_won = user_won + 1
+        print(Fore.LIGHTGREEN_EX)
         print("You have Won..!!")
         print("World still has Hope..!!")
     elif result == 'Lose':
         bella_won = bella_won + 1
+        print(Fore.LIGHTRED_EX)
         print("Alas, you lose..!!")
         print("Please do something ", user_name)
     else:
         tie_num = tie_num + 1
+        print(Fore.LIGHTBLUE_EX)
         print("It is Tie")
 
     print(gap_denoter)
     print(gap_denoter)
 
+    print(Fore.YELLOW)
+    sleep(2)
     print("SCORE BOARD")
     print("Total Round : ", round)
     print("%s points : %d" % (user_name, user_won))
@@ -119,18 +135,28 @@ while round <= 5:
     print("Tie Matches : ", tie_num)
     if round == 5:
         print("OK SO TOTAL SCORE IS UP")
+        sleep(4)
         print("Your score : %d\tBella's score : %d" % (user_won, bella_won))
         print(gap_denoter)
         if bella_won < user_won:
+            print(Fore.LIGHTGREEN_EX)
             print("WE WON THE WIZARDING WAR")
         elif bella_won > user_won:
+            print(Fore.LIGHTRED_EX)
             print("WE LOSE THE WIZARDING WAR")
         else:
+            print(Fore.CYAN)
             print("SCORE UNDECIDED AS IT IS TIED")
     round = round+1
+    print(Fore.YELLOW)
+    print(gap_denoter)
 
-    print(gap_denoter)
-    print(gap_denoter)
+print(Fore.MAGENTA)
+print("="*40)
+print("Name : Swarup Deepak Vishwas".center(40))
+print("Github : @SwarupVishwas18".center(40))
+print("Instagram : @swarup.vishwas".center(40))
+print("Built With Peace 💝".center(40))
+print("="*40)
 
-    print(gap_denoter)
-    print(gap_denoter)
+print(Fore.WHITE)
